1e17198b44f76c1fa1a51b885b180d6d34c9f876
Navarro <joseanp0202@gmail.com>
2024-11-24 17:20:16 +0000

    Modificados themas

--- app/src/main/java/org/iesharia/composeroomapp/ui/theme/Color.kt
+++ app/src/main/java/org/iesharia/composeroomapp/ui/theme/Color.kt
@@ -2,10 +2,23 @@ package org.iesharia.composeroomapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Colores Modo Claro
+val LightBackground = Color(0xFF121212)
+//val LightBackground = Color(0xFFF9F9F9)
+val LightTitle = Color(0xFF1E3A8A)
+val LightText = Color(0xFF333333)
+val LightTextSecondary = Color(0xFF757575)
+val LightButtonAddUpdate = Color(0xFF4CAF50)
+val LightButtonCancel = Color(0xFFE53935)
+val LightSectionBackground = Color(0xFFEDEDED)
+val LightSectionText = Color(0xFF424242)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Colores Modo Oscuro
+val DarkBackground = Color(0xFF121212)
+val DarkTitle = Color(0xFF90CAF9)
+val DarkText = Color(0xFFE0E0E0)
+val DarkTextSecondary = Color(0xFFB0BEC5)
+val DarkButtonAddUpdate = Color(0xFF66BB6A)
+val DarkButtonCancel = Color(0xFFEF5350)
+val DarkSectionBackground = Color(0xFF1E1E1E)
+val DarkSectionText = Color(0xFFCFD8DC)
