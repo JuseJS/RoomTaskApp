59b6cb15f8733e9d70a966208c3e85db5091d38c
Navarro <joseanp0202@gmail.com>
2024-11-24 19:21:40 +0000

    Creado TaskTypeDao.kt y actualizado TaskDao.kt

---
+++ app/src/main/java/org/iesharia/composeroomapp/data/dao/TaskDao.kt
@@ -0,0 +1,27 @@
+package org.iesharia.composeroomapp.data.dao
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import org.iesharia.composeroomapp.data.entity.Task
+
+@Dao
+interface TaskDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: Task)
+
+    @Query("SELECT * FROM tasks ORDER BY id ASC")
+    fun getAllTasks(): Flow<List<Task>>
+
+    @Transaction
+    @Query("SELECT * FROM tasks WHERE id = :taskId")
+    fun getTaskById(taskId: Int): Flow<Task>
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Query("DELETE FROM tasks WHERE id = :taskId")
+    suspend fun deleteTaskById(taskId: Int)
+}
---
+++ app/src/main/java/org/iesharia/composeroomapp/data/dao/TaskTypeDao.kt
@@ -0,0 +1,26 @@
+package org.iesharia.composeroomapp.data.dao
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import org.iesharia.composeroomapp.data.entity.TaskType
+
+@Dao
+interface TaskTypeDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTaskType(taskType: TaskType)
+
+    @Query("SELECT * FROM task_types ORDER BY id ASC")
+    fun getAllTaskTypes(): Flow<List<TaskType>>
+
+    @Query("SELECT * FROM task_types WHERE id = :taskTypeId")
+    fun getTaskTypeById(taskTypeId: Int): Flow<TaskType>
+
+    @Update
+    suspend fun updateTaskType(taskType: TaskType)
+
+    @Delete
+    suspend fun deleteTaskType(taskType: TaskType)
+
+    @Query("DELETE FROM task_types WHERE id = :taskTypeId")
+    suspend fun deleteTaskTypeById(taskTypeId: Int)
+}
