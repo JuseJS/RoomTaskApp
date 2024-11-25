a0ece1e0dcd1878b706f133d1142650f2197d1b7
Navarro <joseanp0202@gmail.com>
2024-11-25 09:24:38 +0000

    Actualizado para funcionar correctamente con el nuevo sistema de navegacion

--- app/src/main/java/org/iesharia/composeroomapp/view/MainActivity.kt
+++ app/src/main/java/org/iesharia/composeroomapp/view/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
 import org.iesharia.composeroomapp.data.database.AppDatabase
+import org.iesharia.composeroomapp.navigation.AppNavigation
 import org.iesharia.composeroomapp.ui.theme.RoomTaskAppTheme
 import org.iesharia.composeroomapp.viewmodel.TaskViewModel
 import org.iesharia.composeroomapp.viewmodel.TaskViewModelFactory
@@ -16,8 +18,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             RoomTaskAppTheme {
                 val database = AppDatabase.getDatabase(this)
-                val viewModel: TaskViewModel = viewModel(factory = TaskViewModelFactory(database.taskDao()))
-                TaskList(viewModel)
+                val taskViewModel: TaskViewModel = viewModel(factory = TaskViewModelFactory(database.taskDao()))
+                val navController = rememberNavController()
+
+                AppNavigation(
+                    navController = navController,
+                    taskViewModel = taskViewModel,
+                    context = this
+                )
             }
         }
     }
