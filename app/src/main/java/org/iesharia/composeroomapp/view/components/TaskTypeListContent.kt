73e596e4b644549b8e0575734264c2ec2956b7a6
Navarro <joseanp0202@gmail.com>
2024-11-25 23:25:24 +0000

    Modificado layout para usar LazyColumn

--- app/src/main/java/org/iesharia/composeroomapp/view/components/TaskTypeListContent.kt
+++ app/src/main/java/org/iesharia/composeroomapp/view/components/TaskTypeListContent.kt
@@ -1,6 +1,9 @@
 package org.iesharia.composeroomapp.view.components
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import org.iesharia.composeroomapp.data.entity.TaskType
 
 @Composable
@@ -8,11 +11,15 @@ fun TaskTypeListContent(
     taskTypes: List<TaskType>,
     onDeleteTaskType: (TaskType) -> Unit,
     onEditTaskType: (TaskType) -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    taskTypes.forEach { taskType ->
-        TaskTypeItem(
-            taskType = taskType,
-            onEdit = { onEditTaskType(taskType) },
-            onDelete = { onDeleteTaskType(taskType) })
+    LazyColumn(modifier = modifier) {
+        items(taskTypes) { taskType ->
+            TaskTypeItem(
+                taskType = taskType,
+                onEdit = { onEditTaskType(taskType) },
+                onDelete = { onDeleteTaskType(taskType) }
+            )
+        }
     }
 }
