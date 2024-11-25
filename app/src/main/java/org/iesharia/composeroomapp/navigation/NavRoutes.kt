39826170717789f3052ffad410f2722aaf181c69
Navarro <joseanp0202@gmail.com>
2024-11-25 23:13:17 +0000

    Corregidas rutas de edicion

--- app/src/main/java/org/iesharia/composeroomapp/navigation/NavRoutes.kt
+++ app/src/main/java/org/iesharia/composeroomapp/navigation/NavRoutes.kt
@@ -2,8 +2,8 @@ package org.iesharia.composeroomapp.navigation
 
 object NavRoutes {
     const val TASKLIST = "taskList"
-    const val ADDTASK = "editTask"
+    const val ADDTASK = "addTask"
     const val EDITTASK = "editTask/{taskId}"
-    const val ADDTASKTYPE = "editTaskType"
+    const val ADDTASKTYPE = "addTaskType"
     const val EDITTASKTYPE = "editTaskType/{taskTypeId}"
 }
